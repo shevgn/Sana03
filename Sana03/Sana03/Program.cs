895daa049530bac22edc76d73c05af58069a186d
Artem Levchenko <evgn.main@gmail.com>
2023-12-08 10:45:00 +0200

    Array generated

--- Sana03/Sana03/Program.cs
+++ Sana03/Sana03/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+int N;
+Console.WriteLine("Enter size N: ");
+if (int.TryParse(Console.ReadLine(), out N))
+{
+    var array = new int[N];
+    var rand = new Random();
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = rand.Next(-20, 20);
+        Console.WriteLine(array[i]);
+    }
+}
